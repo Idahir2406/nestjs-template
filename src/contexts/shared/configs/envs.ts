6a108ca808822bdf194825f1444cef9016ecce4e
Idahir2406 <ipincay5178@utm.edu.ec>
2026-05-03 23:50:09 -0500

    Update database user in docker-compose, downgrade @nestjs/platform-express version, and adjust environment variable schema for PORT coercion

--- src/contexts/shared/configs/envs.ts
+++ src/contexts/shared/configs/envs.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 config();
 const envSchema = z.object({
-  PORT: z.number().default(3000),
+  PORT: z.coerce.number().default(3000),
   DATABASE_URL: z.url(),
   NODE_ENV: z.enum(["development", "production"]).default("development"),
 });
